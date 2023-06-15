b3ac00e5c6ec485154da75ba989162fe0a2f193c
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-06-15 12:08:39 -0700

    Add text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+/**
+ * Display a string using a put function.
+ * Return 0(success)
+ */
 int main(void)
 {
-	/*Displays the given string using the puts function*/
-	puts("Programming is like building a multilingual puzzle");
-	/*Returns the value zero value if success*/
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
