c2f2089d3489b754a447cbddb371a070ef9fd4a5
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-06-15 09:51:22 -0700

    Editing a file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
+	/*Displays the given string using the puts function*/
 	puts("Programming is like building a multilingual puzzle");
+	/*Returns the value zero value if success*/
 	return (0);
 }
