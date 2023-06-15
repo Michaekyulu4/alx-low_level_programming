a65330d5a5328fb67bc20b03ba24779ed31a74fe
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-06-15 09:59:46 -0700

    Edit a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 	/*Displays the given string using the puts function*/
-	puts(""Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	/*Returns the value zero value if success*/
 	return (0);
 }
