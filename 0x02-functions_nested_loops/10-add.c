54e2d8dfaaac72abc32094602bb6008fc6087883
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-06-21 13:48:14 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - check the code
+ * @a: value to be computed
+ * @b: value to be computed
+ * Return:the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
