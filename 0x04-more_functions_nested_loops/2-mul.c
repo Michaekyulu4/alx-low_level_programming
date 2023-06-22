c0d87b8032aaf7df0c548be467bb2ea67995d42c
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-06-22 13:19:56 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Miltiple 2 numbers
+ * @a: the value to be computed
+ * @b: the value to be computed
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
