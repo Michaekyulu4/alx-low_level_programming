79116cf65d6ffd345d174f8d55d6be5a63a47a5c
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-07-05 08:30:49 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string
+ * @s: string to be printed
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
