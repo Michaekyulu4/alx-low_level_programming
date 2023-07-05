928b4247ad94fc9c90ffb931617ba0e91a9001ee
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-07-05 10:09:41 -0700

    rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,13 +6,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+	s = "lpa\0";
+
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _strlen_recursion - A functions that returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
