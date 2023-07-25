d572d1a6af05151f54bc783ef80af727b2428b2b
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-07-25 12:48:19 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * _realloc - reallocates memory location
  * @ptr: former pointer to the old memory allocated
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		p = malloc(new_size)
+		p = malloc(new_size);
 			return (p);
 	}
 	else if (new_size == 0)
