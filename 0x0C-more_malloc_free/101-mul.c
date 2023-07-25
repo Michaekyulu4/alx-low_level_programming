c3698e9f0e87e82f451b36fea2d9a4c37994a7be
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-07-25 16:07:19 -0700

    we must accept

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -38,7 +38,7 @@ int _strlen(char *s)
 /**
  * error - handle main errors
  */
-void error(void)
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
@@ -56,28 +56,28 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		error();
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-		if (!result)
-			return (1);
+	if (!result)
+		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = len2 - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
-		carry += result[len1 + len2 + 1] + (digit1 * digit2);
-		result[len1 + len2 + 1] = carry % 10;
-		carry /= 10;
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-	if (carry > 0)
-		result[len1 + len2 + 1] += carry;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
