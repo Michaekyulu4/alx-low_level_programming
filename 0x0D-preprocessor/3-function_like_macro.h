696fdf63760e45591366a4796d77ca6a5e489e16
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-07-26 08:58:28 -0700

    New macro file

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_FILE
+#define MACRO_FILE
+/**
+ * File: 3-function_like_macro
+ * @x: input value(an int)
+ *
+ * Desc: header file that computes absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
