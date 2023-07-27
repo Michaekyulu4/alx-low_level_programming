7391868a43766e0c57564d3ea213407356998236
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-07-27 09:02:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT
+#define STRUCT
+/**
+ * struct dog - Short description of dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
