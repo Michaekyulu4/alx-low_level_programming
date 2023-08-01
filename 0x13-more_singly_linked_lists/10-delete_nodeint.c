496c0a4ab3c8c08610e4ac9cbe0513b9320753e7
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-08-01 06:51:33 -0700

    Update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,8 +27,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (!temp || !(temp->next))
 			return (-1);
-			temp = temp->next;
-			i++;
+		temp = temp->next;
+		i++;
 	}
 
 
