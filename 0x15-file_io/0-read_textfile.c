5d3ce207880cc6f05a488cefb8732510a6d37cfe
Michaekyulu4 <michaelkyulu4@gmail.com>
2023-08-19 03:27:07 -0700

    new file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and prints it to the POSIX
+ * stdio
+ *
+ * @filename: string pointer
+ * @letters: number of letters printed
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, o_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	nrd = read(fd, buff, letter);
+	nwr = write(STDOUT_FILENO, buff, nrd);
+
+	close(fd);
+	free(buff);
+
+	return (nwr);
+}
